187a12dbc23c8d947fc3811ce68a4444b4eca2ff
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-10-17 22:58:42 +0300

    updated second task

--- Arrays.js
+++ Arrays.js
@@ -1,16 +1,16 @@
 // 1. adding at the end
 
-const addToArray = function (array, string) {
-  console.log(array.push(string));
-  console.log(array);
+const addToArray = function (array1, string) {
+  console.log(array1.push(string));
+  console.log(array1);
 };
 
 addToArray(["Hello", "i", "am"], "John"); // => ["Hello", "i", "am", "John"]
 
 // 2. converting to string
 
-const convertToString = function (array, char) {
-  console.log(array.join(" "));
+const convertToString = function (array1, char) {
+  console.log(array1.join(" "));
 };
 
 convertToString(["Hello", "i", "am", "John"]); // => "Hello i am John"
@@ -18,10 +18,10 @@ convertToString(["Hello", "i", "am", "John"]); // => "Hello i am John"
 // 3. updating an array
 
 // the array values are unique, no duplicate values in the array
-const updateValue = function (array1, index, value) {
-  if (index < array1.length) {
-    array1.splice(index, 1, value);
-    console.log(array1);
+const updateValue = function (array2, index, value) {
+  if (index < array2.length) {
+    array2.splice(index, 1, value);
+    console.log(array2);
   } else {
     console.log("there is no element to updated in this index");
   }
@@ -33,11 +33,18 @@ updateValue([1, 2, 3], 1, 10); // => [1, 10, 3]
 
 // 4. a new array that returns the strings in the old array
 
-const onlyString = function (array2) {
-  //couldn't figure it out yet
+const onlyString = function (array) {
+  const newArray = [];
+  for (let s = 0; s < array.length; s++) {
+    if (typeof array[s] == "string") {
+      newArray.push(array[s]);
+    }
+  }
+  console.log(newArray);
 };
+
 onlyString(["one", 12, "hi", true, 88, "12"]); // => ["one","hi","12"]
-onlyString([10, 20, 30, 40, 50, 60, false]); // => []
+//onlyString([10, 20, 30, 40, 50, 60, false]); // => []
 
 // 5. login function
 
@@ -47,14 +54,13 @@ const users = [
 ];
 
 const login = function (username, password) {
-  if (
-    (username === "Jane" && password === "123456") ||
-    (username === "admin" && password === "abc123")
-  ) {
-    console.log("Login Successful");
-  } else {
-    console.log("Login Failed");
+  for (let u = 0; u < users.length; u++) {
+    if (username === users[u][0] && password === users[u][1]) {
+      return "Loging successful";
+    }
   }
+
+  return "Loging failed";
 };
 
 login("Jane", "123456"); // => "Login Successful"
@@ -77,7 +83,7 @@ maximumNumber([0, 5, 6, 12, 1]); // => 12
 // 7. reversed string
 
 const reversString = function (string) {
-  revStr = [...string].reverse().join(""); //spread syntax
+  revStr = [...string].reverse().join(""); //spread syntax (...)
   console.log(revStr);
 };
 
