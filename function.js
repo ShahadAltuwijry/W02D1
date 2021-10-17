87ddc882983cfb50abcda94c1f3b5b0ae44536f2
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-10-17 14:35:44 +0300

    updated first task

--- function.js
+++ function.js
@@ -1,9 +1,13 @@
+// 1. sumation of two numbers
+
 let sum = function (a, b) {
   sum = a + b;
 };
 console.log(sum);
 
 sum(5, 10);
+
+// 2. average numbers
 
 let average = function (c, d) {
   average = (c + d) / 2;
@@ -13,9 +17,7 @@ console.log(average);
 
 average(20, 5);
 
-/*  
-   5! = 5 * 4 * 3 * 2 * 1
- */
+// 3. factorial
 
 let findFactorial = function (num1) {
   if (num1 == 0 || num1 == 1) {
@@ -28,14 +30,8 @@ let findFactorial = function (num1) {
 num1 = 3;
 findFactorial = findFactorial(num1);
 console.log(findFactorial);
-/*
-Examples:
-findFactorial(1) // => 1
-findFactorial(3) // => 6
-findFactorial(4) // => 24
-findFactorial(6) // => 720
-findFactorial(11) // => 39916800
-*/
+
+// 4. round number
 
 let round = function (number) {
   console.log(Math.round(number));
@@ -44,9 +40,51 @@ let round = function (number) {
 round(0.8);
 console.log(round);
 
+// 5. power of number
+
 let toThePowerOf = function (base, exponent) {
   console.log(Math.pow(base, exponent));
 };
 
 toThePowerOf(2, 3);
 console.log(toThePowerOf);
+
+// 6. random number
+
+const randomNumber = function () {
+  console.log(Math.random());
+};
+
+randomNumber();
+
+// 7. one or zero
+
+let oneOrZero = function (min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(1);
+  console.log(Math.floor(Math.random() * (2 - 0)));
+};
+
+oneOrZero();
+
+// 8. number from a range
+
+const randomRange = function (number) {
+  console.log(Math.floor(Math.random() * number));
+};
+
+randomRange(10);
+
+// 9. include
+
+const includeOf = function (string, character) {
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) === character) {
+      console.log("True");
+    } else {
+      console.log("False");
+    }
+  }
+};
+
+includeOf("Hello", "h"); // => false
